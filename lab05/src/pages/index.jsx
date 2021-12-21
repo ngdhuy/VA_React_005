ad59a018af2d6b9644bbb3c8bf231e9d9dc66bb0
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-12-21 19:55:08 +0700

    feat(lab05): list of Number

---
+++ lab05/src/pages/index.jsx
@@ -0,0 +1,9 @@
+import NumberList from "../components/NumberList";
+export default function Index() {
+  const numbers = [1, 2, 3, 4, 5];
+  return(
+    <>
+      <NumberList numbers={numbers} />
+    </>
+  );
+}
