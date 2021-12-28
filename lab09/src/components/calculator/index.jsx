b9d5905b70dbab6a8caa084c04bbaad93f7c3f6c
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-12-28 21:42:07 +0700

    refactor(lab09): rename component calculator.button

--- lab09/src/components/calculator/index.jsx
+++ lab09/src/components/calculator/index.jsx
@@ -1,17 +1,17 @@
 import { Component, createContext } from "react";
 import Screen from "../calculator.screen";
-import Button from "../calcualotor.button";
-import "./style.css"
+import Button from "../calculator.button";
+import "./style.css";
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      value : 0,
+    this.state = {
+      value: 0,
       isFloat: false,
       memory: undefined,
       operator: undefined,
-      isReset: false
+      isReset: false,
     };
   }
 
@@ -19,39 +19,42 @@ export default class Calculator extends Component {
     let strNewValue = "";
     let newValue = 0;
 
-    switch(value){
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        strNewValue = this.state.isReset == true ? value : this.state.value + value;
-        newValue = this.state.isFloat ? parseFloat(strNewValue) : parseInt(strNewValue);
-        this.setState({ 
-          value : newValue, 
-          isReset : false 
+    switch (value) {
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        strNewValue =
+          this.state.isReset == true ? value : this.state.value + value;
+        newValue = this.state.isFloat
+          ? parseFloat(strNewValue)
+          : parseInt(strNewValue);
+        this.setState({
+          value: newValue,
+          isReset: false,
         });
         break;
-      case 'AC':
-        this.setState({ 
-          value : 0,
+      case "AC":
+        this.setState({
+          value: 0,
           isFloat: false,
           memory: undefined,
           operator: undefined,
-          isReset: false
+          isReset: false,
         });
         break;
       case ".":
-        if(this.state.value.toString().includes('.') == false) {
+        if (this.state.value.toString().includes(".") == false) {
           strNewValue = this.state.value + ".";
-          this.setState({ 
+          this.setState({
             value: strNewValue,
-            isFloat: true 
+            isFloat: true,
           });
         }
         break;
@@ -61,91 +64,91 @@ export default class Calculator extends Component {
         break;
       case "%":
         newValue = (Number(this.state.value) * 100) / 10000;
-        this.setState({ 
-          value: newValue, 
-          isReset: true 
+        this.setState({
+          value: newValue,
+          isReset: true,
         });
         break;
       case "+":
-        if(this.state.memory === undefined) {
+        if (this.state.memory === undefined) {
           newValue = this.state.value;
         } else {
           newValue = this.cal();
         }
 
         this.setState({
-          value : newValue,
-          memory : newValue,
-          operator : "+",
-          isReset: true
+          value: newValue,
+          memory: newValue,
+          operator: "+",
+          isReset: true,
         });
         break;
 
       case "-":
-        if(this.state.memory === undefined) {
+        if (this.state.memory === undefined) {
           newValue = this.state.value;
         } else {
           newValue = this.cal();
         }
 
         this.setState({
-          value : newValue,
-          memory : newValue,
-          operator : "-",
-          isReset: true
+          value: newValue,
+          memory: newValue,
+          operator: "-",
+          isReset: true,
         });
         break;
-      
+
       case "*":
-        if(this.state.memory === undefined) {
+        if (this.state.memory === undefined) {
           newValue = this.state.value;
         } else {
           newValue = this.cal();
         }
 
         this.setState({
-          value : newValue,
-          memory : newValue,
-          operator : "*",
-          isReset: true
+          value: newValue,
+          memory: newValue,
+          operator: "*",
+          isReset: true,
         });
         break;
-      
-        case "/":
-        if(this.state.memory === undefined) {
+
+      case "/":
+        if (this.state.memory === undefined) {
           newValue = this.state.value;
         } else {
           newValue = this.cal();
         }
 
         this.setState({
-          value : newValue,
-          memory : newValue,
-          operator : "/",
-          isReset: true
+          value: newValue,
+          memory: newValue,
+          operator: "/",
+          isReset: true,
         });
         break;
-      
+
       case "=":
-        if(this.state.memory === undefined) {
+        if (this.state.memory === undefined) {
           newValue = this.state.value;
         } else {
           newValue = this.cal();
         }
 
         this.setState({
-          value : newValue,
-          memory : undefined,
-          operator : undefined,
-          isReset: true
+          value: newValue,
+          memory: undefined,
+          operator: undefined,
+          isReset: true,
         });
         break;
       default:
     }
-  }
+  };
 
   cal() {
-    switch(this.state.operator) {
+    switch (this.state.operator) {
       case "+":
         return Number(this.state.memory) + Number(this.state.value);
       case "-":
@@ -153,10 +156,8 @@ export default class Calculator extends Component {
       case "*":
         return Number(this.state.memory) * Number(this.state.value);
       case "/":
-        if(this.state.value === 0)
-          return "N/A";
-        else
-          return Number(this.state.memory) / Number(this.state.value);
+        if (this.state.value === 0) return "N/A";
+        else return Number(this.state.memory) / Number(this.state.value);
       default:
         break;
     }
@@ -164,31 +165,31 @@ export default class Calculator extends Component {
 
   render() {
     const value = this.state.value;
-    return(
+    return (
       <>
         <div>
           <Screen value={value} />
         </div>
         <div className="group-button">
-          <Button value="AC" onHandleClick={this.onClick}/>
-          <Button value="+/-" onHandleClick={this.onClick}/>
-          <Button value="%" onHandleClick={this.onClick}/>
-          <Button value="/" onHandleClick={this.onClick}/>
-          <Button value="7" onHandleClick={this.onClick}/>
-          <Button value="8" onHandleClick={this.onClick}/>
-          <Button value="9" onHandleClick={this.onClick}/>
-          <Button value="X" onHandleClick={this.onClick}/>
-          <Button value="4" onHandleClick={this.onClick}/>
-          <Button value="5" onHandleClick={this.onClick}/>
-          <Button value="6" onHandleClick={this.onClick}/>
-          <Button value="-" onHandleClick={this.onClick}/>
-          <Button value="1" onHandleClick={this.onClick}/>
-          <Button value="2" onHandleClick={this.onClick}/>
-          <Button value="3" onHandleClick={this.onClick}/>
-          <Button value="+" onHandleClick={this.onClick}/>
-          <Button value="0" className="zero" onHandleClick={this.onClick}/>
-          <Button value="." onHandleClick={this.onClick}/>
-          <Button value="=" onHandleClick={this.onClick}/>
+          <Button value="AC" onHandleClick={this.onClick} />
+          <Button value="+/-" onHandleClick={this.onClick} />
+          <Button value="%" onHandleClick={this.onClick} />
+          <Button value="/" onHandleClick={this.onClick} />
+          <Button value="7" onHandleClick={this.onClick} />
+          <Button value="8" onHandleClick={this.onClick} />
+          <Button value="9" onHandleClick={this.onClick} />
+          <Button value="X" onHandleClick={this.onClick} />
+          <Button value="4" onHandleClick={this.onClick} />
+          <Button value="5" onHandleClick={this.onClick} />
+          <Button value="6" onHandleClick={this.onClick} />
+          <Button value="-" onHandleClick={this.onClick} />
+          <Button value="1" onHandleClick={this.onClick} />
+          <Button value="2" onHandleClick={this.onClick} />
+          <Button value="3" onHandleClick={this.onClick} />
+          <Button value="+" onHandleClick={this.onClick} />
+          <Button value="0" className="zero" onHandleClick={this.onClick} />
+          <Button value="." onHandleClick={this.onClick} />
+          <Button value="=" onHandleClick={this.onClick} />
         </div>
       </>
     );
